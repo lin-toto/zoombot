3fd9a679b46c1352f31746efe687ef2e2daeba78
koorimizuw <koorimizuw@yahoo.co.jp>
2021-09-29 20:20:46 +0900

    fix: eslint

--- plugins/auto-leave.js
+++ plugins/auto-leave.js
@@ -1,42 +1,41 @@
-import Timeout from "await-timeout";
+import Timeout from 'await-timeout'
 
-import BasePlugin from "../base-plugin.js";
-import logger from "../logger.js";
+import BasePlugin from '../base-plugin.js'
+import logger from '../logger.js'
 
 class AutoLeave extends BasePlugin {
-  constructor(name, ctx, config) {
-    super(name, ctx, config, {});
-    this.maxParticipantsCount = 0;
-    this.participantsCount = 0;
+  constructor (name, ctx, config) {
+    super(name, ctx, config, {})
+    this.maxParticipantsCount = 0
+    this.participantsCount = 0
   }
 
-  async run() {
+  async run () {
     while (this.running) {
       await this.zoomContext.runExclusive(async () => {
-        logger.debug(this.loggerName + "Fetch participants count");
-        await this.zoomContext.openParticipants();
-        this.participantsCount =
-          await this.zoomContext.fetchParticipantsCount();
+        logger.debug(this.loggerName + 'Fetch participants count')
+        await this.zoomContext.openParticipants()
+        this.participantsCount = await this.zoomContext.fetchParticipantsCount()
         this.maxParticipantsCount = Math.max(
           this.maxParticipantsCount,
           this.participantsCount
-        );
-      });
+        )
+      })
       logger.info(
         this.loggerName +
-          "Participants count:" +
+          'Participants count:' +
           String(this.participantsCount) +
-          "/" +
+          '/' +
           String(this.maxParticipantsCount)
-      );
+      )
       if (this.participantsCount < Math.ceil(this.maxParticipantsCount / 2)) {
-        logger.debug(this.loggerName + "Leave metting...");
-        await this.zoomContext.leave();
-        break;
+        logger.debug(this.loggerName + 'Leave metting...')
+        await this.zoomContext.leave()
+        break
       }
-      await Timeout.set(5000);
+      await Timeout.set(5000)
     }
   }
 }
 
-export default AutoLeave;
+export default AutoLeave
